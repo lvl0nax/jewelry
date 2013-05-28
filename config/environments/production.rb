4bef58381d8fea4118c28837b366e78290a81c9e
lvl0nax <lvl0nax@gmail.com>
2013-05-28 21:32:28 +0400

    fix config for precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ AppTest::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( prettyPhoto.css, chosen.css, facebox.com, style.css, jq1_9.js, script.js, jquery.prettyPhoto.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
