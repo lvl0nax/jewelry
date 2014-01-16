b4c15cf0c9b148cad14e4b3fe618e0c15216a5a4
lvl0nax <lvl0nax@gmail.com>
2014-01-16 21:59:40 +0400

    email

--- app/mailers/card_mailer.rb
+++ app/mailers/card_mailer.rb
@@ -3,7 +3,7 @@ class CardMailer < ActionMailer::Base
   default from: 'bujua'
   def new_order_mail(options)
     @card = options
-    emails = 'lvl0nax@gmail.com,utkin_@hotmail.com'
+    emails = 'lvl0nax@gmail.com'
     # emails = "9856544@gmail.com,akvapolus@restsguide.ru,lider-aqva@metropost.ru"
     mail to: emails, subject: 'Сайт: новый заказ'
   end
