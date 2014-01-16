06d4e3b37d46424ee15fe17e52a64fb2f1257c5b
lvl0nax <lvl0nax@gmail.com>
2014-01-16 22:15:28 +0400

    adding alex email

--- app/mailers/card_mailer.rb
+++ app/mailers/card_mailer.rb
@@ -3,7 +3,7 @@ class CardMailer < ActionMailer::Base
   default from: 'lvl0nax@yandex.ru'
   def new_order_mail(options)
     @card = options
-    emails = 'lvl0nax@gmail.com'
+    emails = 'lvl0nax@gmail.com,utkin_@hotmail.com'
     # emails = "9856544@gmail.com,akvapolus@restsguide.ru,lider-aqva@metropost.ru"
     mail to: emails, subject: 'Сайт: новый заказ'
   end
