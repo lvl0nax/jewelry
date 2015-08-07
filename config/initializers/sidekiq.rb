ebb4ad26e1d2dd426976c3e29f14202b2e356cb7
lvl0nax <lvl0nax@gmail.com>
2015-08-07 21:51:38 +0300

    try to add redis config for sidekiq

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,6 @@
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://redis5.locum.ru:6379' }
+end
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://redis5.locum.ru:6379' }
+end
